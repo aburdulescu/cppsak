3c273a7de276502cc35a985cfb0da914102838f1
Andrei Burdulescu <andrei.burdulescu@pm.me>
2022-03-01 22:31:24 +0200

    Use zig for compilation, working for only for x86_64 linux for now

--- build.bash
+++ build.bash
@@ -14,17 +14,34 @@ fi
 
 rm -rf $builddir
 
-export CCACHE_DIR=/ccache
+[[ $# -lt 1 ]] && echo "target was not provided" && exit 1
+
+case $1 in
+    "linux")
+        zig_target=x86_64-linux-musl
+        llvm_target=X86
+        ;;
+    "windows")
+        zig_target=x86_64-windows-gnu
+        llvm_target=X86
+        ;;
+    *)
+        echo "unsupported target"
+        exit 1
+esac
+
+export CC="zig cc -target $zig_target"
+export CXX="zig c++ -target $zig_target"
 
 cmake \
     -S . \
     -B $builddir \
     -G Ninja \
     -DENUMS_VERSION=$(cat version.txt) \
-    -DLIBCLANG_BUILD_STATIC=ON \
     -DLLVM_ENABLE_PROJECTS="clang" \
+    -DLIBCLANG_BUILD_STATIC=ON \
     -DLLVM_ENABLE_PIC=OFF \
-    -DLLVM_TARGETS_TO_BUILD=X86
+    -DLLVM_TARGETS_TO_BUILD=$llvm_target
 
 cmake --build $builddir --target libclang.a
 cmake --build $builddir --target enums
